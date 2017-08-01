592b940e14b9bbd9c43772eba0afb0336362abf6
Runhao <wangrh1993@gmail.com>
2017-08-01 16:52:07 -0700

    .

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolTest test = new SolTest();
+        test.runTest();
+
+    }
+
+
+
+
+}
